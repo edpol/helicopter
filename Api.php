a7bac414704b80acb2de85c7ba250bebdadf5ecc
Ed Pol <edpol@p4bh.org>
2020-07-20 09:20:41 -0400

    Works on IIS

--- Api.php
+++ Api.php
@@ -11,10 +11,15 @@ class Api {
     private $top_book;
     private $top_fare;
     private $bot;
+    private $opts;
 
 
     public function __construct($AgentLogin, $AgentPassword, $ServiceId)
     {
+        $http = array('method' => 'GET', 'header' => "Content-Type: application/soap+xml\r\n" . "Charset=utf-8\r\n",);
+        $ssl  = array('verify_peer' => false, 'verify_peer_name' => false);
+        $this->opts = array('http' => $http, 'ssl' => $ssl);
+
         $this->top_avail=array();
         $this->top_avail[]='<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:tak="tflite.com/TakeFliteExternalService/" xmlns:ns="http://www.opentravel.org/OTA/2003/05">';
         $this->top_avail[]='    <soap:Header xmlns:wsa="http://www.w3.org/2005/08/addressing">';
@@ -180,71 +185,67 @@ class Api {
      */
     public function getWsdl()
     {
-        $uri = WSDL_ADDR;
-        $wsdl_file = WSDL_FILE;
-
         // if the folder does not exist, create it
         if(!is_dir(WSDL_FOLDER)){
-            if(!mkdir(WSDL_FOLDER, 0777, true)){
+            if(!mkdir(WSDL_FOLDER, 0777, true) && !is_dir(WSDL_FOLDER)){
                 die("Could not create folder " . WSDL_FOLDER . ". Check access permissions. ");
             }
         }
 
         // find out when the cache was last updated
-        if (file_exists($wsdl_file)) {
-            $modified = filemtime($wsdl_file);
+        if (file_exists(WSDL_FILE)) {
+            $modified = filemtime(WSDL_FILE);
         }
 
         // create or update the cache if necessary
         if (!isset($modified) || $modified + CACHE_LIFETIME < time()) {
-            if ($string = file_get_contents(WSDL_ADDR)) {
-                //file_put_contents($wsdl_file, $string);
-                $xml = new SimpleXMLElement($string);
-                $doc = new DOMDocument('1.0', 'utf-8');
+            if($string = @file_get_contents(WSDL_ADDR, false, stream_context_create($this->opts))) {
+                //file_put_contents(WSDL_FILE, $string);
+                $xml = new \SimpleXMLElement($string);
+                $doc = new \DOMDocument('1.0', 'utf-8');
                 $doc->formatOutput = true;
                 $node = dom_import_simplexml($xml);  // convert simpleXML data to DOM node
                 $node = $doc->importNode($node, true);  // import the converted xml can be imported into the empty dom document
                 $doc->appendChild($node);
-                $doc->save($wsdl_file);
+                if(@$doc->save(WSDL_FILE)===false) {
+                    die("Could not create file " . WSDL_FILE . ". Check access permissions. ");
+                }
             } else {
-                die("Could not create file " . WSDL_FILE . ". Check access permissions. ");
+                die("Could not get file " . WSDL_FILE . ". ");
             }
             return true;
         }
 
-        return file_exists($wsdl_file);
+        return file_exists(WSDL_FILE);
     }
 
     public function setupHeader($client)
     {
-        // can we write a function that populates this array?
+        // Don't need this if we get __soapCall to work
         $node_namespace['soap'] = "http://www.w3.org/2003/05/soap-envelope";
         $node_namespace['tak'] = "tflite.com/TakeFliteExternalService/";
         $node_namespace['ns'] = "http://www.opentravel.org/OTA/2003/05";  // for SOAP_1_2
-        $node_namespace['wsa'] = 'http://www.w3.org/2005/08/addressing';
 
         /****************
          * Header
          */
         $action_data = "tflite.com/TakeFliteExternalService/TakeFliteOtaService/OTA_PkgAvailRQ";
         $to_data = 'https://apps8.tflite.com/PublicService/Ota.svc';
-        $action = new \SoapHeader($node_namespace['wsa'], 'Action', $action_data, false);
-        $to = new \SoapHeader($node_namespace['wsa'], 'To', $to_data, false);
+        $wsa = 'http://www.w3.org/2005/08/addressing';
+        $action = new \SoapHeader($wsa, 'Action', $action_data, false);
+        $to = new \SoapHeader($wsa, 'To', $to_data, false);
         return array('Action' => $action, 'To' => $to);
     }
 
     public function instantiateSoapClient(){
         if($this->getWsdl()) {
             ini_set('soap.wsdl_cache_enabled', '0');
-            $http = ['method' => 'GET', 'header' => "Content-Type: application/soap+xml\r\n" . "Charset=utf-8\r\n",];
-            $ssl  = ['verify_peer' => false, 'verify_peer_name' => false];
-            $opts = [ 'http' => $http, 'ssl' => $ssl ];
-            $params = [
+            $params = array(
                 'soap_version' => SOAP_1_2,
                 'cache' => WSDL_CACHE_NONE,
                 'trace' => TRACE,
-                'stream_context' => stream_context_create($opts)
-            ];
+                'stream_context' => stream_context_create($this->opts)
+            );
 
             $client = null;
             try {
@@ -252,7 +253,7 @@ class Api {
                 $headerBody = $this->setupHeader($client);
                 $client->__setSoapHeaders($headerBody);
                 return $client;
-            } catch (SoapFault $e) {
+            } catch (\SoapFault $e) {
                 echo "<pre>";
                 dumpCatch($e, $client);
                 echo "</pre>";
