26e6168a693688a673590d532ee9f4d7f8eebaf9
Ed Pol <edpol@p4bh.org>
2020-08-25 15:50:43 -0400

    added error_reporting

--- initialize.php
+++ initialize.php
@@ -21,10 +21,10 @@ defined('TRACE')          ? null : define('TRACE', true);
 //defined('TRACE')          ? null : define('TRACE', false);
 
 if(file_exists(LIB_PATH.DS.'error_reporting.php')) {
-    require_once(LIB_PATH.'error_reporting.php');
+    require_once(LIB_PATH.DS.'error_reporting.php');
 }
 if(!file_exists(LIB_PATH.DS.'credentials.php')) {
-    die(LIB_PATH.'credentials.php not found');
+    die(LIB_PATH.DS.'credentials.php not found');
 }
 require_once(LIB_PATH.DS.'credentials.php');
 if(!isset($AgentLogin, $AgentPassword, $ServiceId)){
